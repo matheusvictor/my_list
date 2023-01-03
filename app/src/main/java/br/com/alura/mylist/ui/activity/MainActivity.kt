dc4e05a37af907b752190257e20366e6419fc2a4
Matheus Victor <matheusvictor.salles@gmail.com>
2023-01-03 14:58:05 -0300

    feature: adiciona recyclerview na tela principal

--- app/src/main/java/br/com/alura/mylist/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/mylist/ui/activity/MainActivity.kt
@@ -2,12 +2,26 @@ package br.com.alura.mylist.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.mylist.R
+import br.com.alura.mylist.model.Product
+import br.com.alura.mylist.ui.recyclerview.adapter.ProductListAdapter
+import java.math.BigDecimal
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_product_list)
+        recyclerView.adapter = ProductListAdapter(
+            context = this,
+            products = listOf(
+                Product("Example Name", "Example Desc", BigDecimal("1.99")),
+                Product("Example Name", "Example Desc", BigDecimal("1.99")),
+                Product("Example Name", "Example Desc", BigDecimal("1.99")),
+            )
+        )
     }
 }
