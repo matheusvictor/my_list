06dab95f7341be969678fd26f5eceaf7b16a47fe
Matheus Victor <matheusvictor.salles@gmail.com>
2023-01-03 12:55:28 -0300

    move para pacote de UI

---
+++ app/src/main/java/br/com/alura/mylist/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.alura.mylist.ui.activity
+
+import android.app.Activity
+import android.os.Bundle
+import br.com.alura.mylist.R
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
