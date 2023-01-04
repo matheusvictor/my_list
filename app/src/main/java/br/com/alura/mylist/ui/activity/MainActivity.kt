8ef80057b58addee712cdb3651e0ce6f640fc043
Matheus Victor <matheusvictor.salles@gmail.com>
2023-01-04 11:28:05 -0300

    refactor: adiciona ação para clique do botão flutante

--- app/src/main/java/br/com/alura/mylist/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/mylist/ui/activity/MainActivity.kt
@@ -1,11 +1,13 @@
 package br.com.alura.mylist.ui.activity
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.mylist.R
 import br.com.alura.mylist.model.Product
 import br.com.alura.mylist.ui.recyclerview.adapter.ProductListAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.math.BigDecimal
 
 class MainActivity : Activity() {
@@ -23,5 +25,15 @@ class MainActivity : Activity() {
                 Product("Example Name", "Example Desc", BigDecimal("1.99")),
             )
         )
+
+        clickOnFloatingButton()
+    }
+
+    private fun clickOnFloatingButton() {
+        val fab = findViewById<FloatingActionButton>(R.id.floating_add_item)
+        fab.setOnClickListener {
+            val intentFormActivity = Intent(this, ProductFormActivity::class.java)
+            startActivity(intentFormActivity)
+        }
     }
 }
