3e2d60d7cd5dbe306a1d352b422611e8d9d1772c
Matheus Victor <matheusvictor.salles@gmail.com>
2023-01-04 12:06:22 -0300

    feat: adiciona produto à lista de dados em memória
    - Refatora ação de criar produto para método
    - Add método para finalizar activity após adicionar produto

--- app/src/main/java/br/com/alura/mylist/ui/activity/ProductFormActivity.kt
+++ app/src/main/java/br/com/alura/mylist/ui/activity/ProductFormActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.mylist.R
+import br.com.alura.mylist.dao.ProductsDAO
 import br.com.alura.mylist.model.Product
 import java.math.BigDecimal
 
@@ -23,32 +24,42 @@ class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
         val confirmButton = findViewById<Button>(R.id.bt_form_confirm)
         confirmButton.setOnClickListener {
 
-            val fieldProductName = findViewById<EditText>(R.id.form_product_name)
-            val productName = fieldProductName.text.toString()
-            Log.i("ProductFormActivity", "onCreate: $productName")
+            val newProduct = createNewProduct()
 
-            val fieldProductDescription = findViewById<EditText>(R.id.form_product_description)
-            val productDescription = fieldProductDescription.text.toString()
-            Log.i("ProductFormActivity", "onCreate: $productDescription")
+            val productsDAO = ProductsDAO()
+            productsDAO.add(newProduct)
+            Log.i("ProductFormActivity", "onCreate: ${productsDAO.findAll()}")
 
-            val fieldProductPrice = findViewById<EditText>(R.id.form_product_price)
-            val productPriceString = fieldProductPrice.text.toString()
-
-            val productPrice = if (productPriceString.isNotBlank()) {
-                BigDecimal(productPriceString)
-            } else {
-                BigDecimal.ZERO
-            }
-
-            val newProduct = Product(
-                productName = productName,
-                description = productDescription,
-                price = productPrice
-            )
-
-            Log.i("ProductFormActivity", "onCreate: $newProduct")
+            finish() // finaliza activity e remove da pilha
         }
 
+    }
+
+    private fun createNewProduct(): Product {
+        val fieldProductName = findViewById<EditText>(R.id.form_product_name)
+        val productName = fieldProductName.text.toString()
+        Log.i("ProductFormActivity", "onCreate: $productName")
+
+        val fieldProductDescription = findViewById<EditText>(R.id.form_product_description)
+        val productDescription = fieldProductDescription.text.toString()
+        Log.i("ProductFormActivity", "onCreate: $productDescription")
+
+        val fieldProductPrice = findViewById<EditText>(R.id.form_product_price)
+        val productPriceString = fieldProductPrice.text.toString()
+
+        val productPrice = if (productPriceString.isNotBlank()) {
+            BigDecimal(productPriceString)
+        } else {
+            BigDecimal.ZERO
+        }
+
+        val newProduct = Product(
+            productName = productName,
+            description = productDescription,
+            price = productPrice
+        )
+        return newProduct
+        Log.i("ProductFormActivity", "onCreate: $newProduct")
     }
 
 }
