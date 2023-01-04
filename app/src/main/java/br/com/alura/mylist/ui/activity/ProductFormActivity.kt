ebafa66f227f1233bda143b03a375214080c0666
Matheus Victor <matheusvictor.salles@gmail.com>
2023-01-04 09:09:16 -0300

    feature: adiciona activity para formulário de produto

---
+++ app/src/main/java/br/com/alura/mylist/ui/activity/ProductFormActivity.kt
@@ -0,0 +1,14 @@
+package br.com.alura.mylist.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.alura.mylist.R
+
+class ProductFormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_form)
+    }
+
+}
