22790cddd3c49635d773e6b51421f9ad89c1df06
Matheus Victor <matheusvictor.salles@gmail.com>
2023-01-04 10:06:58 -0300

    refactor: converte para data class

--- app/src/main/java/br/com/alura/mylist/model/Product.kt
+++ app/src/main/java/br/com/alura/mylist/model/Product.kt
@@ -2,7 +2,7 @@ package br.com.alura.mylist.model
 
 import java.math.BigDecimal
 
-class Product(
+data class Product(
     val productName: String,
     val description: String,
     val price: BigDecimal
