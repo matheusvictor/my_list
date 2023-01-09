32b3d64c260cfb193585804cad83e192aeb86e4d
Matheus Victor <matheusvictor.salles@gmail.com>
2023-01-09 01:49:54 -0300

    feat/wip: adiciona ação para exclusão de item

--- app/src/main/java/br/com/alura/mylist/repository/dao/ProductDAO.kt
+++ app/src/main/java/br/com/alura/mylist/repository/dao/ProductDAO.kt
@@ -1,8 +1,10 @@
 package br.com.alura.mylist.repository.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import br.com.alura.mylist.model.Product
 
 @Dao
@@ -13,5 +15,8 @@ interface ProductDAO {
 
     @Insert
     fun save(vararg product: Product)
+
+    @Delete
+    fun delete(vararg product: Product)
 
 }
--- app/src/main/java/br/com/alura/mylist/ui/activity/ProductDetailsActivity.kt
+++ app/src/main/java/br/com/alura/mylist/ui/activity/ProductDetailsActivity.kt
@@ -2,18 +2,22 @@ package br.com.alura.mylist.ui.activity
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.mylist.R
 import br.com.alura.mylist.databinding.ActivityProductDetailsBinding
 import br.com.alura.mylist.extension.formatToRealCurrency
 import br.com.alura.mylist.extension.tryLoadImage
 import br.com.alura.mylist.model.Product
+import br.com.alura.mylist.repository.AppDatabase
 
 class ProductDetailsActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityProductDetailsBinding.inflate(layoutInflater)
     }
+
+    private lateinit var product: Product
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +30,29 @@ class ProductDetailsActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (::product.isInitialized) {
+            val db = AppDatabase.getInstance(this)
+            val productDao = db.productDao()
+
+            when (item.itemId) {
+                R.id.menu_details_edit_product -> {
+
+                }
+                R.id.menu_details_delete_product -> {
+                    productDao.delete(product)
+                    finish()
+                }
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun tryLoadProduct() {
         intent.getParcelableExtra<Product>(CHAVE_PRODUTO)?.let {
+            this.product = it
             fillFields(it)
         } ?: finish()
     }
